a8cf47c10330f3bfea61535cf1be1f7ae44ea826
Almer <129676227+AlmerCarbonEquity@users.noreply.github.com>
2024-02-14 12:51:41 +0100

    simplify further

--- model_bakery_one_to_one/core/baker_recipes.py
+++ model_bakery_one_to_one/core/baker_recipes.py
@@ -1,13 +1,10 @@
 from model_bakery.recipe import Recipe
 from django.contrib.auth.models import User
 from .models import Person
-from .models import Profile
 from model_bakery.recipe import foreign_key
 
 user = Recipe(User)
 
 person = Recipe(Person)
-
-profile = Recipe(Profile)  
 
 user_with_person = user.extend(person=foreign_key(person))
